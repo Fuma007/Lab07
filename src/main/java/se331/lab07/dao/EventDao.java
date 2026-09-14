474819f61c02e8d53372bc2bcc0df04f54f70d4f
Fuma007 <techit_wongkiti@cmu.ac.th>
2026-09-14 15:07:56 +0700

    5.2 commit

--- src/main/java/se331/lab07/dao/EventDao.java
+++ src/main/java/se331/lab07/dao/EventDao.java
@@ -1,4 +1,11 @@
 package se331.lab07.dao;
 
+import se331.lab07.entity.Event;
+
+import java.util.List;
+
 public interface EventDao {
+    Integer getEventSize();
+    List<Event> getEvents(Integer pageSize, Integer page);
+    Event getEvent(Long id);
 }
