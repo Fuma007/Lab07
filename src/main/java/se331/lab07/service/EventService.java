8759ab44074ef13ab18c4828cb84152031e49eb5
Fuma007 <techit_wongkiti@cmu.ac.th>
2026-09-14 15:08:58 +0700

    5.3 commit

--- src/main/java/se331/lab07/service/EventService.java
+++ src/main/java/se331/lab07/service/EventService.java
@@ -1,4 +1,11 @@
 package se331.lab07.service;
 
-public class EventService {
+import se331.lab07.entity.Event;
+
+import java.util.List;
+
+public interface EventService {
+    Integer getEventSize();
+    List<Event> getEvents(Integer pageSize, Integer page);
+    Event getEvent(Long id);
 }
