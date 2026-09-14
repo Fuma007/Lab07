c3dbdc2969c705d304acd823be46dc2c175e8178
Fuma007 <techit_wongkiti@cmu.ac.th>
2026-09-14 14:28:23 +0700

    2.8 commit

--- src/main/java/se331/lab07/controller/EventController.java
+++ src/main/java/se331/lab07/controller/EventController.java
@@ -1,5 +1,7 @@
 package se331.lab07.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se331.lab07.entity.Event;
 
@@ -10,6 +12,11 @@ import java.util.List;
 @RestController
 public class EventController {
     List<Event> eventList;
+
+    @GetMapping("/events")
+    public ResponseEntity<?> getEventLists() {
+        return ResponseEntity.ok(eventList);
+    }
 
     @PostConstruct
     public void init() {
